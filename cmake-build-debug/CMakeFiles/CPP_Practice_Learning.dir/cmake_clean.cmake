348fe8969b7dc464c2c0965fb3fd88126748674a
Mekrou <mek.rmills@gmail.com>
2019-09-07 22:25:40 -0400

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/CPP_Practice_Learning.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPP_Practice_Learning.dir/main.cpp.obj"
+  "CPP_Practice_Learning.pdb"
+  "CPP_Practice_Learning.exe"
+  "CPP_Practice_Learning.exe.manifest"
+  "libCPP_Practice_Learning.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPP_Practice_Learning.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/CPP_Practice_Learning.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CPP_Practice_Learning.
+# This may be replaced when dependencies are built.
