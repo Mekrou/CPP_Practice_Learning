afa91c84f3526651cb560bc6aa0b97d8f8752693
Mekrou <mek.rmills@gmail.com>
2019-09-08 14:30:50 -0400

    Created Notes & Practice directory. Started C++ operators notes.

---
+++ Notes_Practice/operators.cpp
@@ -0,0 +1,17 @@
+//
+// Created by ryan0 on 9/8/2019.
+//
+// Insertion and Extraction operators practice.
+//
+
+#include <iostream>
+
+int operators()
+{
+    std::cout << "Hello World" << std::endl;
+
+    
+
+    return 0;
+}
+
