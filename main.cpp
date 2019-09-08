5f075dbb8d03d13a68c6b2730e9c3bcfcb35b61b
Mekrou <mek.rmills@gmail.com>
2019-09-07 22:42:12 -0400

    Edited main.cpp to a simple favorite-number program.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main()
+{
+    std::cout << "Enter your favorite number between 1-100: " << std::endl;
+
+    // 'u' means user
+    int u_favorite_number;
+    std::cin >> u_favorite_number;
+
+    if (u_favorite_number <= 100 && u_favorite_number >= 1) {
+        std::cout << "Amazing! " << u_favorite_number << " is my favorite number too!" << std::endl;
+    } else {
+        std::cout << "Invalid Input! Program exiting... " << std::endl;
+    }
+
     return 0;
 }
